2d77386203561b10bc1fa54c1a633ae251d5b393
Weylin Wagnon <weylinwagnon@gmail.com>
2017-02-20 21:57:25 -0800

    finish upload l

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const path = require('path');
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer( (req, res) => {
+  console.log(req.url);
   if (req.url === '/') {
     fs.createReadStream('./index.html').pipe(res);
   }
-  else if (req.url.match(/^\/file\//)) {
-    if (req.method === "GET") return fileCtrl.sendFile(req, res);
-    if (req.method === "POST") return fileCtrl.saveFile(req, res);
+  else if (req.url.match(/^\/file/)) {
+    const pieces = req.url.split('/');
+    console.log(pieces);
+    console.log(pieces.length);
+    if (req.method === "GET") {
+      if (pieces && pieces[2]) return fileCtrl.sendFile(req, res, pieces[2]);
+      return fileCtrl.sendFileList(req, res);
+    } 
+    else if (req.method === "POST") {
+      return fileCtrl.uploadFile(req, res, pieces[2]);
+    }
   }
   else {
       res.end('<h1>not found!</h1>');
